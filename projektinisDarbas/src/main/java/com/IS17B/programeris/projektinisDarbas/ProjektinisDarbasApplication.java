bde675c06aba79b4d60f11d04ac252b9fb12944a
VirmantasArmusevicius <58297737+VirmantasArmusevicius@users.noreply.github.com>
2019-12-09 17:24:36 +0200

    Add files via upload

---
+++ projektinisDarbas/src/main/java/com/IS17B/programeris/projektinisDarbas/ProjektinisDarbasApplication.java
@@ -0,0 +1,13 @@
+package com.IS17B.programeris.projektinisDarbas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektinisDarbasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektinisDarbasApplication.class, args);
+	}
+
+}
---
+++ projektinisDarbas/src/test/java/com/IS17B/programeris/projektinisDarbas/ProjektinisDarbasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IS17B.programeris.projektinisDarbas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektinisDarbasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
