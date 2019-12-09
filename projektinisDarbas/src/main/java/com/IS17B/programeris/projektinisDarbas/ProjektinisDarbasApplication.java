a94817b2d6b24a02515c59c40535bc7945dd0849
Filipas Moskaliovas <filipasmoskaliovas@gmail.com>
2019-12-09 20:19:44 +0200

    Commit

--- projektinisDarbas/src/main/java/com/IS17B/programeris/projektinisDarbas/ProjektinisDarbasApplication.java
+++ projektinisDarbas/src/main/java/com/IS17B/programeris/projektinisDarbas/ProjektinisDarbasApplication.java
@@ -2,6 +2,7 @@ package com.IS17B.programeris.projektinisDarbas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ProjektinisDarbasApplication {
@@ -9,5 +10,7 @@ public class ProjektinisDarbasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjektinisDarbasApplication.class, args);
 	}
+
+
 
 }
